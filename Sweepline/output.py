a2147072be56eb62042a403032b3a05d694bdafb
yezi2016 <zhuyezi@hotmail.com>
2017-01-10 21:19:09 +0100

    sweepline

---
+++ Sweepline/output.py
@@ -0,0 +1,102 @@
+import os
+import matplotlib.pyplot
+import sweepline
+from shapes import Point, Edge, Direction
+from timeit import default_timer as timer
+
+def show(p):
+    for tuple in p.edges:
+        edge = tuple[0]
+        matplotlib.pyplot.plot([edge.get_left_point().x, edge.get_right_point().x],
+                 [edge.get_left_point().y, edge.get_right_point().y], linestyle="-", linewidth = 2, color="r" if tuple[1] else "g")
+
+def read(openfile):
+    loc = os.path.dirname(os.path.realpath(__file__))
+    with open(os.path.join(loc, openfile), 'r') as f:
+        content = f.readlines()
+        f.closed
+    return content
+
+#Delete blanklines of infile
+def delete_blanklines():
+    infp = open('data.txt', "r")
+    outfp = open('data_1.txt', "w")
+    lines = infp.readlines()
+    for li in lines:
+        if li.split():
+            outfp.writelines(li)
+    infp.close()
+    outfp.close()
+
+def edge_list(points):
+    former_point = None
+    edges = []
+    for text in points:
+        Line = text.split()
+        if len(Line) == 1 and points[0] == text:
+            None
+        else:
+            point = Point(int(Line[0]), int(Line[1]))
+            if former_point != None:
+                edges.append(Edge(former_point, point, Direction.Right))
+            former_point = point
+    edges.append(Edge(edges[len(edges)-1].q, edges[0].p, Direction.Right))
+    return edges
+
+def Dict(text, dist):
+    min_x = 0
+    i = 0
+    for text in text:
+        l = text.split()
+        if (len(l) == 1) and (text[0] == text):
+            min_x = int(l[0])
+        elif len(l) > 1:
+            if i < min_x:
+                dist[i] = [l[0], l[1]]
+                i += 1
+    if (i < min_x):
+        print("Need points!")
+        dist.clear()
+
+#Begin to record the running time
+timing=0.0
+start_time = timer()
+delete_blanklines()
+# According to the value of data to adjust the final result visualization here
+adjust =1
+# Construct edges list
+edges = edge_list(read('data_1.txt'))
+# Calculate limits
+# put coordinates of any point here
+# Initinalize
+line = edges[0]
+minx = line.p.x
+maxx = line.p.x
+miny = line.p.y
+maxy = line.p.y
+for line in edges:
+    minx = min(minx, line.p.x, line.q.x)
+    maxx = max(maxx, line.p.x, line.q.x)
+    miny = min(miny, line.p.y, line.q.y)
+    maxy = max(maxy, line.p.y, line.q.y)
+
+# Create bounding box
+minx = minx-adjust
+maxx = maxx+adjust
+miny = miny-adjust
+maxy = maxy+adjust
+
+# Construct trapezoidal map
+td = sweepline.trapezoid_decompose(edges)
+# End timing
+end_time = timer()
+timing=start_time-end_time
+if timing:
+    print('Running time: {0:.4f}'.format(end_time - start_time))
+
+#Visualization of results
+matplotlib.pyplot.axes()
+matplotlib.pyplot.ylim([miny, maxy])
+matplotlib.pyplot.xlim([minx, maxx])
+show(td)
+matplotlib.pyplot.show()
---
+++ Sweepline/sweepline.py
@@ -0,0 +1,99 @@
+from enum import IntEnum
+from bintrees import avltree
+from shapes import Edge,Status, Direction, Decomposition
+
+class Event:
+    def __init__(self, edge, type, index):
+        self.edge = edge
+        self.type = type
+        self.index = index
+
+class EventType(IntEnum):
+    Removal = 1
+    Insert = 2
+    Nothing = 3
+
+def build_event_queue(edges):
+    tree = avltree.AVLTree() # avltree is a subclass of BinarySearchTree
+    events = []
+    for edge in edges:
+        if edge.p.x < edge.q.x:
+            events.append(Event(edge, EventType.Insert, edge.p.x))
+            events.append(Event(edge, EventType.Removal, edge.q.x))
+        else:
+            events.append(Event(edge, EventType.Removal, edge.p.x))
+            events.append(Event(edge, EventType.Insert, edge.q.x))
+    events = sorted(events, key=lambda evt: (evt.index, evt.type))
+    index = -1
+    evts = []
+
+    for evt in events:
+        if evt.index == index:
+            evts.append(evt)
+        else:
+            if len(evts) > 0:
+                if type == EventType.Insert:
+                    tree.insert(index, evts)
+                else:
+                    tree.insert(index, evts)
+
+            evts = []
+            index = evt.index
+            evts.append(evt)
+    tree.insert(index, evts)
+    return tree
+
+def add_edges(status, targetX, edge, trapezoid_decomposition):
+    f = open('output.txt', 'w')
+    upper = None
+    lower = None
+    key = Status(edge.point_at_edge(targetX).y, 0)
+    try:
+        upper = status.ceiling_item(key)
+    except KeyError:
+        None
+    try:
+        lower = status.floor_item(key)
+    except KeyError:
+        None
+    if upper != None and ((upper[1].is_left_to_right() and upper[1].insideOn == Direction.Right) or
+                              (upper[1].is_right_to_left() and upper[1].insideOn == Direction.Left)):
+        i = "({}, {}),".format(edge.point_at_edge(targetX).x, edge.point_at_edge(targetX).y)
+        j = "({}, {})".format(upper[1].point_at_edge(targetX).x, upper[1].point_at_edge(targetX).y)
+        print('Added Edge :' + str(i + j))
+        f.write(str(i + j) + '\n')
+        trapezoid_decomposition.add_vertex_edge(Edge(edge.point_at_edge(targetX), upper[1].point_at_edge(targetX), Direction.Both))
+    if lower != None and ((lower[1].is_right_to_left() and lower[1].insideOn == Direction.Right) or
+                              (lower[1].is_left_to_right() and lower[1].insideOn == Direction.Left)):
+        i = "({}, {}),".format(edge.point_at_edge(targetX).x, edge.point_at_edge(targetX).y)
+        j = "({}, {})".format(lower[1].point_at_edge(targetX).x, lower[1].point_at_edge(targetX).y)
+        print('Added Edge :' + str(i + j))
+        f.write(str(i + j) + '\n')
+        trapezoid_decomposition.add_vertex_edge(Edge(edge.point_at_edge(targetX), lower[1].point_at_edge(targetX), Direction.Both))
+
+
+def trapezoid_decompose(edges):
+    # Construct event queue firstly
+    event_queue = build_event_queue(edges)
+    # Initialize status
+    status = avltree.AVLTree()
+    # Handle events
+    trapezoid_decomposition = Decomposition()
+    while len(event_queue) > 1:
+        event_T = event_queue.pop_min()
+        draw_td_edge = False
+        for evt in event_T[1]:
+            targetX = evt.index
+            if not draw_td_edge and (evt.type == EventType.Insert):
+                add_edges(status, targetX, evt.edge, trapezoid_decomposition)
+                draw_td_edge = True
+            if evt.type == EventType.Insert:
+                status.insert(evt.edge.statusForEdge(), evt.edge)
+                trapezoid_decomposition.add_edge(evt.edge)
+            if evt.type == EventType.Removal:
+                status.remove(evt.edge.statusForEdge())
+        if not draw_td_edge:
+            add_edges(status, targetX, event_T[1][0].edge, trapezoid_decomposition)
+            draw_td_edge = True
+        print("Status size is {}".format(len(status)))
+    return trapezoid_decomposition
