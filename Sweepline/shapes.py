2baee915085bf5afc18619b66826880af92a6c04
Zico2017 <zhuyezi@hotmail.com>
2017-01-10 19:51:53 +0100

    incompleted

--- Sweepline/shapes.py
+++ Sweepline/shapes.py
@@ -1,3 +1,4 @@
+from enum import Enum
 class Point:
     def __init__(self, x, y):
         self.x = x
@@ -52,6 +53,10 @@ class Edge:
         dx = self.get_right_point().x - self.get_left_point().x
         dy = self.get_right_point().y - self.get_left_point().y
         return dy/dx
+    def statusKeyForEdge(self):
+        dx = self.get_right_point().x - self.get_left_point().x
+        dy = self.get_right_point().y - self.get_left_point().y
+        return StatusKey(self.get_left_point().y, dy / dx)
 
     def point_at_edge(self, targetX):
         return Point(targetX, self.get_left_point().y + (targetX - self.get_left_point().x) * self.edge_slope())
@@ -61,3 +66,27 @@ class Edge:
 
     def is_right_to_left(self):
         return self.p.x > self.q.x
+
+
+class StatusKey:
+    def __init__(self, left_point_y, dxdy):
+        self.left_point_y = left_point_y
+        self.dxdy = dxdy
+
+    def __repr__(self):
+        return "(start: {}, dxdy: {})".format(self.left_point_y, self.dxdy)
+
+
+class Direction(Enum):
+    Left = 1
+    Right = 2
+    Both = 3
+
+class VerticalDecomposition:
+    # list of tuples (a, b) where a is an edge in the decomposition and b is true iff a is an edge of the original
+    # polygon.
+    edges = []
+    def addEdge(self, edge):
+        self.edges.append((edge, True))
+    def addVertEdge(self, edge):
+        self.edges.append((edge, False))
