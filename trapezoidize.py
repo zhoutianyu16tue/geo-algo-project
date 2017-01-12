0a899094e363cd94ebeb3a701c12c0cffb1d9d5c
tianyu <hitmann47@tianyus-MacBook-Pro.local>
2017-01-12 16:56:09 +0100

    fixing naming issues

--- trapezoidize.py
+++ trapezoidize.py
@@ -1,18 +1,13 @@
-from point import Point
-from edge import Edge
-from random import shuffle, seed
 from trapezoidalMap import TrapezoidalMap
 from searchGraph import SearchGraph
 from trapezoid import Trapezoid
-import sys
-
-Epsilon = 1e-5
 
 class Trapezoidize():
 
-    def __init__(self, points):
+    def __init__(self, edges, boundingBox):
         self.trapezoids = []
-        self.edges, self.boundingBox = self.connectPoints(points)
+        self.edges = edges
+        self.boundingBox = boundingBox
         self.trapezoidalMap = TrapezoidalMap(self.boundingBox)
         self.searchGraph = SearchGraph((self.boundingBox.node))
  
@@ -31,19 +26,19 @@ class Trapezoidize():
                 cq = edge.q.inside(trapezoid)
 
                 if cp and cq:
-                    tlist = self.trapezoidalMap.trapezoidContainEdge(trapezoid, edge)
-                    self.searchGraph.trapezoidContainEdge(trapezoid.node, edge, tlist)
+                    newTrapezoids = self.trapezoidalMap.trapezoidContainEdge(trapezoid, edge)
+                    self.searchGraph.trapezoidContainEdge(trapezoid.node, edge, newTrapezoids)
                 elif cp and not cq:
-                    tlist = self.trapezoidalMap.trapezoidContainLeftEndpint(trapezoid, edge) 
-                    self.searchGraph.trapezoidContainLeftEndpint(trapezoid.node, edge, tlist)
+                    newTrapezoids = self.trapezoidalMap.trapezoidContainLeftEndpint(trapezoid, edge) 
+                    self.searchGraph.trapezoidContainLeftEndpint(trapezoid.node, edge, newTrapezoids)
                 elif not cp and not cq:
-                    tlist = self.trapezoidalMap.trapezoidCrossedByEdge(trapezoid, edge)
-                    self.searchGraph.trapezoidCrossedByEdge(trapezoid.node, edge, tlist)
+                    newTrapezoids = self.trapezoidalMap.trapezoidCrossedByEdge(trapezoid, edge)
+                    self.searchGraph.trapezoidCrossedByEdge(trapezoid.node, edge, newTrapezoids)
                 else:
-                    tlist = self.trapezoidalMap.trapezoidContainRightEndpint(trapezoid, edge)
-                    self.searchGraph.trapezoidContainRightEndpint(trapezoid.node, edge, tlist)
+                    newTrapezoids = self.trapezoidalMap.trapezoidContainRightEndpint(trapezoid, edge)
+                    self.searchGraph.trapezoidContainRightEndpint(trapezoid.node, edge, newTrapezoids)
                 # Add new trapezoids to map
-                for t in tlist:
+                for t in newTrapezoids:
                     self.trapezoidalMap.map[t.hashCode] = t
 
             self.trapezoidalMap.bcross = None
@@ -62,46 +57,3 @@ class Trapezoidize():
     def markOutside(self, t):
         if t.top is self.boundingBox.top or t.bottom is self.boundingBox.bottom:
             t.trimNeighbors()
-  
-    def connectPoints(self, points):
-        edgeList = []
-        numberOfPoints = len(points)
-        for idx, point in enumerate(points):
-            next = idx + 1 if idx < numberOfPoints-1 else 0
-            p = points[idx][0], points[idx][1]
-            q = points[next][0], points[next][1]
-
-            # if p[0] == q[0]:
-            #     sys.exit('Detect vertical edge!')
-
-            edgeList.append((p, q))
-
-        # use the infinite box as the bounding box
-        maxX = sys.maxsize
-        maxY = maxX
-        minX = -sys.maxsize + 1
-        minY = minX
-        top = Edge(Point(minX, maxY), Point(maxX, maxY))
-        bottom = Edge(Point(minX, minY), Point(maxX, minY))
-        left = Point(minX, maxY)
-        right = Point(maxX, maxY)
-
-        return self.randomizeEdgeOrder(edgeList), Trapezoid(left, right, top, bottom)
-  
-    def randomizeEdgeOrder(self, edgeList):
-        edges = []
-        for e in edgeList:
-            p = self.shearTransformation(e[0])
-            q = self.shearTransformation(e[1])
-
-            if p.x > q.x:
-                edges.append(Edge(q, p))
-            else:
-                edges.append(Edge(p, q))
-        
-        seed()
-        shuffle(edges)
-        return edges
-
-    def shearTransformation(self, point):
-        return Point(point[0] + Epsilon * point[1], point[1])
