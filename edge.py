71b303a5bd93401a257c2468779c1e53271a71e1
tianyu <hitmann47@tianyus-MacBook-Pro.local>
2017-01-12 17:12:28 +0100

    nothing

--- edge.py
+++ edge.py
@@ -1,8 +1,15 @@
 class Edge():
     
     def __init__(self, p, q):
-        self.p = p
-        self.q = q
+
+        # point p always has smaller x coordinate
+        if q.x < p.x:
+            self.p = q
+            self.q = p
+        else:
+            self.p = p
+            self.q = q
+
         self.slope = (q.y - p.y) / (q.x - p.x) if q.x != p.x else 0
         self.b = p.y - (p.x * self.slope)
         self.above, self.below = None, None
--- main.py
+++ main.py
@@ -14,20 +14,25 @@ from trapezoid import Trapezoid
 # star = generatePolygon(250, 250, 200, .8, .8, 2)
 Epsilon = 1e-5
 
-def connectPoints(points):
-    edgeList = []
+def makeRandomOrderEdges(points):
+    edges = []
     numberOfPoints = len(points)
+
     for idx, point in enumerate(points):
         next = idx + 1 if idx < numberOfPoints-1 else 0
-        p = points[idx][0], points[idx][1]
-        q = points[next][0], points[next][1]
 
-        # if p[0] == q[0]:
-        #     sys.exit('Detect vertical edge!')
+        # Shear transformation
+        p = Point(points[idx][0] + Epsilon * points[idx][1], points[idx][1])
+        q = Point(points[next][0] + Epsilon * points[next][1], points[next][1])
 
-        edgeList.append((p, q))
-
-    return randomizeEdgeOrder(edgeList)
+        if p.x > q.x:
+            edges.append(Edge(q, p))
+        else:
+            edges.append(Edge(p, q))
+    
+    seed()
+    shuffle(edges)
+    return edges
 
 def makeBoundingBox():
     # use the infinite box as the bounding box
@@ -42,28 +47,10 @@ def makeBoundingBox():
 
     return Trapezoid(left, right, top, bottom)
 
-def randomizeEdgeOrder(edgeList):
-    edges = []
-    for e in edgeList:
-        p = shearTransformation(e[0])
-        q = shearTransformation(e[1])
-
-        if p.x > q.x:
-            edges.append(Edge(q, p))
-        else:
-            edges.append(Edge(p, q))
-    
-    seed()
-    shuffle(edges)
-    return edges
-
-def shearTransformation(point):
-    return Point(point[0] + Epsilon * point[1], point[1])
-
 def main():
     
-    star = [(73, 224), (65, 183), (36, 120), (110, 120), (127, 74), (171, 62), (215, 72), (253, 37), (295, 56), (334, 71), (373, 90), (419, 105), (433, 149), (411, 200), (427, 234), (457, 276), (464, 325), (419, 356), (372, 367), (353, 405), (320, 424), (283, 443), (241, 475), (203, 434), (178, 400), (132, 402), (69, 401), (67, 346), (85, 298), (38, 265)] 
-    trapezoidize = Trapezoidize(connectPoints(star), makeBoundingBox())
+    star = [(-55, 460), (-211, 250), (-43, 29), (125, -206), (376, -126), (620, 12), (648, 250), (558, 476), (363, 654), (149, 570)]
+    trapezoidize = Trapezoidize(makeRandomOrderEdges(star), makeBoundingBox())
 
     trapezoids = trapezoidize.trapezoids
 
